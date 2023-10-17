11f1de463208c48f882e7e9ff905c952cea10464
Anirud Lappathi <anirud.lappathi@gmail.com>
2023-10-16 21:37:13 -0500

    Added CSS

--- datalayer/clothes_db.py
+++ datalayer/clothes_db.py
@@ -34,47 +34,47 @@ def create_cloth(clothing_type, color, is_clean):
         print(f"Create Cloth Error: {e}")
 
 # Read (Select) Users
-def get_all_users():
-    try:
-        select_query = "SELECT * FROM clothes"
-        cursor.execute(select_query)
-        clothes = cursor.fetchall()
-        return clothes
-    except mysql.connector.Error as err:
-        print(f"Get All Users Error: {err}")
-        return None
+# def get_all_users():
+#     try:
+#         select_query = "SELECT * FROM clothes"
+#         cursor.execute(select_query)
+#         clothes = cursor.fetchall()
+#         return clothes
+#     except mysql.connector.Error as err:
+#         print(f"Get All Users Error: {err}")
+#         return None
 
-def get_user_by_id(user_id):
-    try:
-        select_query = "SELECT * FROM clothes WHERE id = %s"
-        cursor.execute(select_query, (user_id,))
-        user = cursor.fetchone()
-        return user
-    except mysql.connector.Error as err:
-        print(f"Get User By ID Error: {err}")
-        return None
+# def get_user_by_id(user_id):
+#     try:
+#         select_query = "SELECT * FROM clothes WHERE id = %s"
+#         cursor.execute(select_query, (user_id,))
+#         user = cursor.fetchone()
+#         return user
+#     except mysql.connector.Error as err:
+#         print(f"Get User By ID Error: {err}")
+#         return None
 
-# Update User Information
-def update_user(user_id, new_username, new_first_name, new_last_name, new_email, new_phone_number, new_user_photo_file_name):
-    try:
-        update_query = "UPDATE clothes SET user_id = %s username = %s, first_name = %s, last_name = %s, email = %s, phone_number = %s, user_photo_file_name = %s WHERE id = %s"
-        data = (user_id, new_username, new_first_name, new_last_name, new_email, new_phone_number, new_user_photo_file_name, user_id)
-        cursor.execute(update_query, data)
-        conn.commit()
-        print("User updated successfully.")
-    except mysql.connector.Error as err:
-        print(f"Update User Error: {err}")
+# # Update User Information
+# def update_user(user_id, new_username, new_first_name, new_last_name, new_email, new_phone_number, new_user_photo_file_name):
+#     try:
+#         update_query = "UPDATE clothes SET user_id = %s username = %s, first_name = %s, last_name = %s, email = %s, phone_number = %s, user_photo_file_name = %s WHERE id = %s"
+#         data = (user_id, new_username, new_first_name, new_last_name, new_email, new_phone_number, new_user_photo_file_name, user_id)
+#         cursor.execute(update_query, data)
+#         conn.commit()
+#         print("User updated successfully.")
+#     except mysql.connector.Error as err:
+#         print(f"Update User Error: {err}")
 
-# Delete User
-def delete_user(user_id):
-    try:
-        delete_query = "DELETE FROM clothes WHERE id = %s"
-        cursor.execute(delete_query, (user_id,))
-        conn.commit()
-        print("User deleted successfully.")
-    except mysql.connector.Error as err:
-        print(f"Delete User Error: {err}")
+# # Delete User
+# def delete_user(user_id):
+#     try:
+#         delete_query = "DELETE FROM clothes WHERE id = %s"
+#         cursor.execute(delete_query, (user_id,))
+#         conn.commit()
+#         print("User deleted successfully.")
+#     except mysql.connector.Error as err:
+#         print(f"Delete User Error: {err}")
 
-def close_connection():
-    cursor.close()
-    conn.close()
+# def close_connection():
+#     cursor.close()
+#     conn.close()
