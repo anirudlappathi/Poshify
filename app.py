e372a650e1b53e8dfbac6fe8fa4a0996abe278b7
Anirud Lappathi <anirud.lappathi@gmail.com>
2023-10-16 22:34:10 -0500

    added stuff idek tbh

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from datalayer import clothes_db
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('src/home.html')
 
-@app.route('/put_clothes', methods=['POST'])
+@app.route('/home.html', methods=['POST'])
 def get_clothes():
     if request.method == 'POST':
         clothing_type = request.form['clothing_type']
         color = request.form['color']
         is_clean = request.form['is_clean']
         result = clothes_db.create_cloth(clothing_type, color, is_clean)
-        return render_template('result.html', result=result)
+        return redirect(url_for('index'))
 
 
