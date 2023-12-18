6c0c43c8b8473e1b90190fccb89bb65f61ce032e
Anirud Lappathi <anirud.lappathi@gmail.com>
2023-12-17 22:27:11 -0800

    Streamlined setting up the db

--- create_table_clothes.py
+++ create_table_clothes.py
@@ -1,24 +1,98 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Boolean
-from datalayer.database import engine
+import mysql.connector
 
-def SetClothesDB():
-    metadata = MetaData(bind=engine)
-    existing_table = Table('Clothes', metadata, autoload=True)
+# ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` `
+# ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` `
+# ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` `
+# CHANGE THESE LINES IN THE LIST AND RUN TO CONFIGURE YOUR LOCAL DB
+# ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
+# ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
+# ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
 
-    # ADD LATER TO CHECK AND ADD ALL COLUMNS
-    columns_to_add = [
-        Column('hue', Integer),
-        Column('saturation', Integer),
-        Column('value', Integer),
-        Column('tone', Integer),
-        Column('colortemp', Integer)
-    ]
+USERS_COMMANDS = [
+    "user_id INT PRIMARY KEY",
+    "username VARCHAR(255)",
+    "first_name VARCHAR(255)",
+    "last_name VARCHAR(255)",
+    "email VARCHAR(255)",
+    "phone_number VARCHAR(15)",
+    "user_photo_file_name VARCHAR(255)"
+]
 
-    for column in columns_to_add:
-        if not hasattr(existing_table.c, column.name):
-            column.create(existing_table)
+CLOTHES_COMMANDS = [
+    "clothes_id INT",
+    "MODIFY COLUMN clothes_id INT AUTO_INCREMENT",
+    "ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES Users(user_id);",
+    "clothing_type VARCHAR(255)",
+    "is_clean TINYINT(1)",
+    "color VARCHAR(255)",
+    "hue INT",
+    "saturation INT",
+    "value INT",
+    "tone VARCHAR(255)",
+    "colortemp VARCHAR(255)",
+    "clothing_name VARCHAR(255)",
+]
 
-    metadata.create_all(engine)
+# ONLY CHANGE THESE IF WE ADD MORE DB's
+
+TABLES = {
+    "Users" : USERS_COMMANDS,
+    "Clothes" : CLOTHES_COMMANDS
+}
+
+password = input("MYSQL Password: ")
+password = password.strip()
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=password
+)
+
+cursor = connection.cursor()
+
+def init_database():
+    try:
+        cursor.execute(f"USE Poshify")
+        print(f"Sucess: Accessed 'Poshify'")
+    except mysql.connector.Error as err:
+        cursor.execute(f"CREATE DATABASE Poshify")
+        cursor.execute(f"USE Poshify")
+        print(f"Sucess: Creating 'Poshify'")
+
+def init_table(table_name):
+    try:
+        cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
+        if cursor.fetchone() is None:
+            cursor.execute(f"CREATE TABLE {table_name};")
+            cursor.execute(f"ALTER TABLE {table_name};")
+            print(f"Creating '{table_name}': Success")
+        else:
+            print(f"Success: Accessing '{table_name}'")
+    except Exception as e:
+        print(f"Error: init_table {e}")
+
+def init_column(table_name, command):
+    try:
+        cursor.execute(f"ALTER TABLE {table_name} MODIFY COLUMN {command};")
+        print(f"Success: Changed data type of column '{command.split()[1]} in {table_name}")
+    except mysql.connector.Error as err:
+        try:
+            cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {command};")
+            print(f"Success: Added {command} to {table_name}.")
+        except mysql.connector.Error as err:
+            print(f"Failure: Cannot add or modify column {command} to {table_name}.")
+
+def create_database():
+
+    init_database()
+    for table_name in TABLES:
+        init_table(table_name)
+        for command in TABLES[table_name]:
+            init_column(table_name, command)
 
 if __name__ == "__main__":
-    SetClothesDB()
+    create_database()
+
+connection.commit()
+cursor.close()
+connection.close()
