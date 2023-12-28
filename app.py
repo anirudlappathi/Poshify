f32e9dd1d344e8bd066bcbb7768a8c04ca452341
Anirud Lappathi <anirud.lappathi@gmail.com>
2023-12-27 16:16:12 -0800

    fixed closet bugs and image processing brightness

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from datalayer.clothes_db import *
 from datalayer.users_db import *
 from algorithm.color_algo import GetStyleOutfits
-from algorithm.color_detection_camera import process_image
+from algorithm.color_detection_camera import dominant_color_finder_dataurl
 
-from PIL import Image
-import io
 import base64
+from PIL import Image
+from io import BytesIO
 
 import json
 import os
@@ -46,24 +46,25 @@ def home():
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
    return oauth.auth0.authorize_redirect(
-      redirect_uri=url_for("callback", _external=True),
+      redirect_uri=url_for("callback_signup", _external=True),
       screen_hint="signup"
    )
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
    return oauth.auth0.authorize_redirect(
-      redirect_uri=url_for("callback", _external=True),
+      redirect_uri=url_for("callback_login", _external=True),
       screen_hint="login"
    )
 
-@app.route("/callback", methods=["GET", "POST"])
-def callback():
+@app.route("/callback_login", methods=["GET", "POST"])
+def callback_login():
    token = oauth.auth0.authorize_access_token()
    session["user"] = token
+   session["userid"] = token['userinfo']['sub'][14:]
    print(session.get("user"))
-   
-   user_id = token['userinfo']['sub'][14:]
+   print('LOGIN')
+   user_id = session.get('userid')
    print(user_id)
    username = token['userinfo']['nickname']
    password = None
@@ -72,6 +73,42 @@ def callback():
    email = token['userinfo']['email']
    phone_number = None
    user_photo_file_name = token['userinfo']['picture']
+   
+   #create_user(user_id=user_id, username=username, password=password, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, user_photo_file_name=user_photo_file_name)   
+
+   return redirect("/dashboard")
+
+@app.route("/callback_signup", methods=["GET", "POST"])
+def callback_signup():
+   print('SIGNUP')
+
+   token = oauth.auth0.authorize_access_token()
+   session["user"] = token
+   session["userid"] = token['userinfo']['sub'][14:]
+   
+   missingInfo = []
+
+   user_id = session.get("userid")
+   user_photo_file_name = token['userinfo']['picture']
+   email = token['userinfo']['email']
+
+   if 'nickname' not in token['userinfo']:
+      missingInfo.append('nickname')
+   else:
+      username = token['userinfo']['nickname']
+
+   if 'given_name' not in token['userinfo']:
+      missingInfo.append('given_name')
+   else:
+      first_name = token['userinfo']['given_name']
+
+   if 'family_name' not in token['userinfo']:
+      missingInfo.append('family_name')
+   else:
+      last_name = token['userinfo']['family_name']
+
+   phone_number = ""
+   password = ""
    
    create_user(user_id=user_id, username=username, password=password, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, user_photo_file_name=user_photo_file_name)   
 
@@ -97,11 +134,11 @@ def dashboard():
    user = session.get("user")
    if not user:
       print("ERROR: USER NOT LOGGED IN")
-      return render_template("home.html", session=session.get('user'))
-   user_id = user['userinfo']['sub'][14:]
+      return render_template("home.html", session=user)
+   user_id = session.get('userid')
    if not user_id:
       print("ERROR: NO ID_TOKEN FOUND")
-      return render_template("home.html", session=session.get('user'))
+      return render_template("home.html", session=user)
    return render_template("dashboard.html", session=user, user_id=user_id, pretty=json.dumps(session.get('user'), indent=4))
 
 @app.route("/closet", methods=["POST", "GET"])
@@ -109,11 +146,11 @@ def closet():
       user = session.get("user")
       if not user:
          print("ERROR: USER NOT LOGGED IN")
-      user_id = user['userinfo']['sub'][14:]
+      user_id = session.get('userid')
       if not user_id:
          print("ERROR: NO ID_TOKEN FOUND")
 
-      clothes = get_clothing_name_and_image_by_user_id(user_id)
+      clothes = get_clothing_name_image_id_by_user_id(user_id)
       print(clothes)
       return render_template("closet.html", session=user, user_id=user_id, clothes=clothes)
 
@@ -123,7 +160,7 @@ def generate_fit():
    if not user:
       print("ERROR: USER NOT LOGGED IN")
       return render_template("home.html", session=session.get('user'))
-   user_id = user['userinfo']['sub'][14:]
+   user_id = session.get('userid')
    if not user_id:
       print("ERROR: NO ID_TOKEN FOUND")
       return render_template("home.html", session=session.get('user'))
@@ -145,7 +182,7 @@ def settings():
    if not user:
       print("ERROR: USER NOT LOGGED IN")
       return render_template("home.html", session=session.get('user'))
-   user_id = user['userinfo']['sub'][14:]
+   user_id = session.get('userid')
    if not user_id:
       print("ERROR: NO ID_TOKEN FOUND")
       return render_template("home.html", session=session.get('user'))
@@ -158,7 +195,7 @@ def add_clothing_manual():
    if not user:
       print("ERROR: USER NOT LOGGED IN")
       return render_template("home.html", session=session.get('user'))
-   user_id = user['userinfo']['sub'][14:]
+   user_id = session.get('userid')
    if not user_id:
       print("ERROR: NO ID_TOKEN FOUND")
       return render_template("home.html", session=session.get('user'))
@@ -168,79 +205,73 @@ def add_clothing_manual():
       clothing_name = request.form['clothing_name']
       clothing_type = request.form['clothes_type']
       is_clean = request.form['is_clean'] == "y"
-      hue = request.form['hue']
-      saturation = request.form['saturation']
-      value = request.form['value']
 
       image_data = request.files['image']
+
       if image_data.filename == '':
          return render_template("add_clothing_manual.html", result="No Selected File", session=user, user_id=user_id)   
       
-      if image_data:
-         filename = str(uuid.uuid4()) + os.path.splitext(image_data.filename)[1]
-         image_data.save(os.path.join('static/clothing_images', filename))
+      filename = str(uuid.uuid4()) + os.path.splitext(image_data.filename)[1]
+      image_data.save(os.path.join('static/clothing_images', filename))
+
+      file_path = os.path.join("static/clothing_images", filename)
+      with open(file_path, 'rb') as img_file:
+         image_binary = img_file.read()
+
+      encoded_image = base64.b64encode(image_binary)
+      dominant_color = dominant_color_finder_dataurl(encoded_image)
+
+      hue = dominant_color[0]
+      saturation = dominant_color[1]
+      value = dominant_color[2]
 
       if clothing_name is None or clothing_type is None or is_clean is None or hue is None or saturation is None or value is None:
-         return render_template("add_clothing_manual.html", result="Empty Field", user_id=user_id)       
+         return render_template("add_clothing_manual.html", session=user, result="Empty Field", user_id=user_id)       
 
       result = create_cloth(user_id, clothing_name, clothing_type, is_clean, hue, saturation, value, filename)
       return render_template("add_clothing_manual.html", result=result, session=user, user_id=user_id)   
    
-   return render_template("add_clothing_manual.html", result="", user_id=user_id)               
+   return render_template("add_clothing_manual.html", session=user, result="", user_id=user_id)               
 
 @app.route("/add_clothing_camera", methods=["POST", "GET"])
 def add_clothing_camera():
-   print("CODE RUNNING")
    user = session.get("user")
    if not user:
       print("ERROR: USER NOT LOGGED IN")
       return render_template("home.html", session=session.get('user'))
-   user_id = user['userinfo']['sub'][14:]
+   user_id = session.get('userid')
    if not user_id:
       print("ERROR: NO ID_TOKEN FOUND")
       return render_template("home.html", session=session.get('user'))
    print(request.method)
    if request.method == 'POST':
-        print("hello")
-        clothing_name = request.form['clothing_name']
-        clothing_type = request.form['clothes_type']
-        is_clean = request.form['is_clean']
+      clothing_name = request.form['clothing_name']
+      clothing_type = request.form['clothes_type']
+      is_clean = request.form['is_clean']
 
-        # Get the base64-encoded image data from the form
-        image_data = request.form['imageData']
-        print("IMAGE DATA: " + image_data)
+      if (is_clean == "y"):
+         is_clean = True
+      else:
+         is_clean = False
 
+      image_data = request.form['imageData']
 
-        # Process the image (example: get the dominant color)
-        dominant_color = process_image(image_data)
-        print("dominant color: " + str(dominant_color))
+      # 360 100 100
+      hue, saturation, value = dominant_color_finder_dataurl(image_data)
+      filename = None
 
-        hue = dominant_color[0]
-        saturation = dominant_color[1]
-        value = dominant_color[2]
+      if image_data:
+         filename = str(uuid.uuid4()) + ".jpeg"
+         image_binary = base64.b64decode(image_data)
+         img = Image.open(BytesIO(image_binary))
+         img.save(os.path.join('static/clothing_images', filename), "JPEG")
+      else:
+         return render_template("add_clothing_camera.html", result="Camera Data invalid or not working", user_id=user_id)   
 
-        image_bytes = base64.b64decode(image_data)
+      result = create_cloth(user_id, clothing_name, clothing_type, is_clean, hue, saturation, value, filename)
 
-        # Generate a unique filename with an appropriate extension (e.g., .jpg)
-        filename = str(uuid.uuid4()) + ".jpeg"
-
-      # Define the file path where you want to save the image
-        file_path = os.path.join('', filename) ## THIS CODE NOT WORKING - ANIRUD PLS MAKE IT WORK 
-
-      # Write the image bytes to a file
-        with open(file_path, 'wb') as file:
-         file.write(image_bytes)
-
-        if (is_clean == "y"):
-           is_clean = True
-        else:
-           is_clean = False
-         
-
-        result = create_cloth(user_id, clothing_name, clothing_type, is_clean, hue, saturation, value, file_path)
-
-        return render_template("add_clothing_camera.html", result=result, user_id=user_id)   
-    
+      return render_template("add_clothing_camera.html", session=user, result=result, user_id=user_id)   
+   
    return render_template("add_clothing_camera.html", session=user, user_id=user_id)               
 
 
@@ -251,11 +282,9 @@ def update_element():
         updated_text = data.get('updatedText')
         identifier = data.get('identifier')
         user = session.get("user")
-        user_id = user['userinfo']['sub'][14:]
+        user_id = session.get('userid')
         print("USER ID: " + user_id)
 
-        # Perform database update or any other necessary operation with updated_text
-        # You can use the identifier to identify which element was edited
         update_clothing_name_by_identifier(identifier, updated_text, user_id)
 
         print("Updated Text:", updated_text)
@@ -272,23 +301,20 @@ def update_element():
 
 @app.route('/delete', methods=['DELETE'])
 def delete_element():
-    if request.method == 'DELETE':
-        data = request.get_json()
-        clothing_name = data.get('clothingName')
-        user = session.get("user")
-        user_id = user['userinfo']['sub'][14:]
-
-        delete_clothing_by_name(clothing_name, user_id)
-        
-        print("Deleted Clothing:", clothing_name)
-
-        # Returning a success message
-        response_data = {
-            'message': f'{clothing_name} deleted successfully'
-        }
-        return jsonify(response_data), 200
-    else:
-        return 'Invalid request', 400
+   if request.method == 'DELETE':
+      data = request.get_json()
+      clothes_id = data.get('clothes_id')
+      user_id = session.get('userid')
+      url = get_clothing_url_by_id(clothes_id, user_id)
+      file_path = os.path.join("static/clothing_images", url)
+      if os.path.exists(file_path):
+            os.remove(file_path)
+            delete_clothing_by_id(clothes_id, user_id)
+            return jsonify({'message': f'{clothes_id} deleted successfully'}), 200
+      else:
+            return jsonify({'message': 'File not found'}), 404
+   else:
+      return jsonify({'message': 'Method not allowed'}), 405
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=81)
