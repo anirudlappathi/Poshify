2df7759f014c593c9f14a747d5a97de86eb2f4c8
Anirud Lappathi <anirud.lappathi@gmail.com>
2024-01-03 22:11:24 -0800

    prevented large file uploads

---
+++ static/add_clothing_manual.js
@@ -0,0 +1,12 @@
+function checkFileSize(event) {
+    const file = event.target.files[0];
+    const maxFileSize = 100 * 1024; // 100 KB
+    const messageBox = document.getElementById("jsMessageBox");
+    console.log('working');
+    if (file && file.size > maxFileSize) {
+        event.target.value = '';
+        messageBox.textContent = "File exceeds max file size (100KB)";
+    } else {
+        messageBox.textContent = "";
+    }
+}
